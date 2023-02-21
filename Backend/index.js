78854721d360e7eb14af628797fe75a7f46c78f1
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-21 16:38:40 +0530

    adding register route for users

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const { connect } = require("./config/db");
+const { userRouter } = require("./routes/user.routes");
 const app = express();
 require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("This is the Home Page ");
+});
+app.use(express.json());
+app.use("/user", userRouter);
+
 app.listen(process.env.port, async () => {
   try {
     await connect;
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,1 +1,47 @@
-//
+const express = require("express");
+const userRouter = express();
+const bcrypt = require("bcrypt");
+const { UserModel } = require("../model/user.model");
+
+// for register the user ---->
+userRouter.post("/register", async (req, res) => {
+  const { firstName, lastName, email, password, gender, mobile } = req.body;
+  const newuser = await UserModel.find({ email });
+  if (newuser.length != 0) {
+    res.status(500).json({
+      message: "This user is already signup ,Please login",
+      status: "Failed",
+    });
+    return;
+  }
+  try {
+    bcrypt.hash(password, 6, async (err, hash_pass) => {
+      if (err) {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: "Something went wrong", status: "Failed" });
+      } else {
+        const user = new UserModel({
+          firstName,
+          lastName,
+          email,
+          password: hash_pass,
+          gender,
+          mobile,
+        });
+        await user.save();
+        res
+          .status(201)
+          .json({ message: "User register successfully", status: "Success" });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message, status: "Failed" });
+  }
+});
+
+module.exports = {
+  userRouter,
+};
