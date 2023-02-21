f9491e548852addb457a62f22e681b9491764e38
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-21 16:20:04 +0530

    connecting to datbase

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const { connect } = require("./config/db");
 const app = express();
 require("dotenv").config();
-app.listen(process.env.port, () => {
+app.listen(process.env.port, async () => {
+  try {
+    await connect;
+    console.log("connected to db");
+  } catch (error) {
+    console.log(error);
+  }
   console.log("server running at 8080");
 });
