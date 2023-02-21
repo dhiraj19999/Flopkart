731ca7443303402a6d949ba6e5e7001e2047ad12
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-21 22:33:32 +0530

    adding get by id route

--- Backend/routes/products.routes.js
+++ Backend/routes/products.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ProductModel } = require("../model/products.model");
 const productRouter = express.Router();
 
+// get products --->
 productRouter.get("/", async (req, res) => {
   const limit = req.query.limit || 10;
   const page = Math.max(0, req.query.page || 0);
@@ -20,6 +21,19 @@ productRouter.get("/", async (req, res) => {
     const product = await ProductModel.find()
       .limit(limit)
       .skip(limit * page);
+    res.status(201).json({ data: product, status: "Success" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong", status: "Failed" });
+  }
+});
+
+// get product by id
+
+productRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await ProductModel.find({ _id: id });
     res.status(201).json({ data: product, status: "Success" });
   } catch (error) {
     console.log(error);
