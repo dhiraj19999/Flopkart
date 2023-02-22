dd2f26c7770e824c4436e6d6adc63baf31f6b1e2
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-02-23 01:49:00 +0530

    added footer

--- Client/flopcart/src/App.js
+++ Client/flopcart/src/App.js
@@ -1,11 +1,21 @@
+// import { Box } from "@chakra-ui/react";
+import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 }
 
 export default App;
+
+// Delete once home page is added
+/* 
+<Box h="500px" w="100%" bgColor="yellow">
+  Home Page
+</Box>
+*/
