62b7d11572858dab1f3925bc5b484b1e02a23e91
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-22 16:52:10 +0530

    adding cart schema for api docs

--- Backend/routes/cart.routes.js
+++ Backend/routes/cart.routes.js
@@ -2,6 +2,62 @@ const express = require("express");
 const { CartModel } = require("../model/cart.model");
 const cartRouter = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Cart:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *          description: The auto-generated id of the product
+ *        uniq_id:
+ *          type: string
+ *          description: A unique id of the product
+ *        product_url:
+ *          type: string
+ *          description: flipkart url for the product
+ *        product_name:
+ *          type: string
+ *          description: name of the product
+ *        pid:
+ *          type: string
+ *          description: product id
+ *        retail_price:
+ *           type: integer
+ *           description: retail price of the product
+ *        discounted_price:
+ *           type: string
+ *           description: discounted price of the product
+ *        image:
+ *           type: string
+ *           description: image of the product
+ *        is_FK_Advantage_product:
+ *           type: string
+ *        description:
+ *           type: string
+ *           description: description of the product
+ *        product_rating:
+ *           type: string
+ *           description: rating of the product
+ *        overall_rating:
+ *           type: string
+ *           description: overall rating of the product
+ *        brand:
+ *           type: string
+ *           description: name of the brand
+ *        product_specifications:
+ *           type: string
+ *           description: extra specs about the product
+ *        authorID:
+ *            type: string
+ *            description: id of the user who add this product in the cart
+ *        quantity:
+ *            type: integer
+ *            description: default quantity will be 1
+ */
+
 cartRouter.get("/", async (req, res) => {
   console.log(req.body);
   try {
