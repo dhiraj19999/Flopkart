f9286d936687a5a03c01073410a022b2c9f976ab
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-24 14:59:09 +0530

    adding product page

--- Client/flopcart/src/Components/ProductPage/Productlist.jsx
+++ Client/flopcart/src/Components/ProductPage/Productlist.jsx
@@ -1,5 +1,6 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React, { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getData } from "../../redux/dataReducer/action";
@@ -9,6 +10,7 @@ export const Productlist = () => {
   const dispatch = useDispatch();
   const { name } = useParams();
   const { isLoading, products } = useSelector((store) => store.dataReducer);
+  const [active, setActive] = useState("pop");
   useEffect(() => {
     dispatch(getData(name));
     console.log(products);
@@ -24,14 +26,43 @@ export const Productlist = () => {
       </Text>
       <div className={styles.sort_container}>
         <Text fontWeight={"500"}>Sort By</Text>
-        <Text>Popularity</Text>
-        <Text>Price --Low to high</Text>
-        <Text>Price --High to low</Text>
+        <Text
+          _active={{ color: "blue" }}
+          onClick={() => setActive("pop")}
+          color={`${active == "pop" ? "#2874F0" : ""}`}
+          fontWeight={`${active == "pop" ? "500" : ""}`}
+          borderBottom={`${active == "pop" ? "2px solid #2874F0" : ""}`}
+          paddingBottom="5px"
+          cursor={"pointer"}
+        >
+          Popularity
+        </Text>
+        <Text
+          onClick={() => setActive("low")}
+          color={`${active == "low" ? "#2874F0" : ""}`}
+          fontWeight={`${active == "low" ? "500" : ""}`}
+          borderBottom={`${active == "low" ? "2px solid #2874F0" : ""}`}
+          paddingBottom="5px"
+          cursor={"pointer"}
+        >
+          Price --Low to high
+        </Text>
+        <Text
+          onClick={() => setActive("high")}
+          color={`${active == "high" ? "#2874F0" : ""}`}
+          fontWeight={`${active == "high" ? "500" : ""}`}
+          borderBottom={`${active == "high" ? "2px solid #2874F0" : ""}`}
+          paddingBottom="5px"
+          cursor={"pointer"}
+        >
+          Price --High to low
+        </Text>
       </div>
-
-      {products?.map((el) => {
-        return <ProductCard {...el} />;
-      })}
+      <div className={styles.products_box}>
+        {products?.map((el, i) => {
+          return <ProductCard {...el} key={i} />;
+        })}
+      </div>
     </div>
   );
 };
