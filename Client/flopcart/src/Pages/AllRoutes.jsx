f50ca1ae4ab7a5499880cd2f8a4c55fd1c95d91f
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-02-26 19:44:15 +0530

    corrections

--- Client/flopcart/src/Pages/AllRoutes.jsx
+++ Client/flopcart/src/Pages/AllRoutes.jsx
@@ -6,12 +6,9 @@ import Home from "./Home/Home";
 import Product from "./ProductPage/Product";
 import SingleProduct from "./SingleProduct/SingleProduct";
 import UserAccount from "./UserAccount/UserAccount";
-<<<<<<< HEAD
+
 import UserAccountAdmin from "./Admin/UserAccountAdmin";
-=======
-import AdninProduct from "../Components/Admin/AdminProduct";
-import { NewHome } from "../Components/NewHome/NewHome";
->>>>>>> b9ffb9e437e0755ecd141bec5a9edec10dc5d7ec
+
 const AllRoutes = () => {
   return (
     <Routes>
@@ -33,11 +30,8 @@ const AllRoutes = () => {
       <Route path="/myprofile" element={<UserAccount />}>
         Checkout
       </Route>
-<<<<<<< HEAD
+
       <Route path="/admin" element={<UserAccountAdmin />} />
-=======
-      <Route path="/Adminsingle/:id" element={<AdninProduct />} />
->>>>>>> b9ffb9e437e0755ecd141bec5a9edec10dc5d7ec
     </Routes>
   );
 };
